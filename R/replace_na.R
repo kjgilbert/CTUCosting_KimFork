ea8fade721b46116affd31fc13d489811ba97775
root <root@NB-24-027-0.ispm.unibe.ch>
2026-06-18 08:17:53 +0200

    try again to fix the replace_na

--- R/replace_na.R
+++ R/replace_na.R
@@ -1,4 +1,16 @@
-#' @noRd
+#' Replace NA values with "NA" value
+#'
+#' This utility function catches NA values inside structural inputs and 
+#' replaces them with an "NA".
+#'
+#' @param x A vector or value to check for NA.
+#'
+#' @return "NA" if input was NA.
+#' @export
+#'
+#' @examples
+#' replace_na(NA)
+#' replace_na("Valid String")
 replace_na <- function(x){
   if(is.na(x)) x <- "NA"
   return(x)
